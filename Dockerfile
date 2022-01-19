bfc3258b90b088fdfa95efc2abce7184751ec534
ksr-at-289154328210 <seetharamkoya9@gmail.com>
2022-01-19 19:07:04 +0530

    update nodemon to watch

--- Dockerfile
+++ Dockerfile
@@ -14,6 +14,6 @@ COPY . .
 
 #Expose port and start application
 EXPOSE 3000
-CMD [ "npm", "start" ]
+CMD [ "npm", "run","dev"]
 
 
--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let port = 3000
 
 // Defines request response in root URL (/)
 app.get('/', function (req, res) {
-res.send('Hello World! from Nodejs in Docker container run by "SEETHARAM KOYA"');
+res.send('Hello World! from Nodejs in Docker container run by "SEETHARAM KOYA!!!!"');
 }).listen(port);
 
 console.log("Serverrunning at- http://localhost:%d", port);
